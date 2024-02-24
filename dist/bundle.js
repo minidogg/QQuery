67794fc584e7e8d77e8ea25946a031c9baed7f07
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-02-24 10:23:34 -0600

    Built docs

--- dist/bundle.js
+++ dist/bundle.js
@@ -89,3 +89,13 @@ Element.prototype.toggle = function() {
     return this
 };
 
+
+//html.js
+/**
+ * Allows easy getting and setting of an elements innerHTML
+ * @returns {InnerHTML} - Returns an elements innerHTML
+ */
+Element.prototype.html = function(html=undefined) {
+    if(html!=undefined)this.innerHTML = html
+    return this.innerHTML
+};
