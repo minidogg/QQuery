3959ff084b7363e9666d292022bb8221663bf73e
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-02-24 10:14:08 -0600

    made visibility chaining easier

--- src/js/visibility.js
+++ src/js/visibility.js
@@ -1,20 +1,26 @@
 /**
  * Extends element prototype with a function to make an element hidden
+ * @returns {Element} - Returns the element, allowing for chaining of other utils.
  */
 Element.prototype.hide = function() {
     this.style.visibility = "hidden";
+    return this
 };
 
 /**
  * Extends element prototype with a function to make an element visible
+ * @returns {Element} - Returns the element, allowing for chaining of other utils.
  */
 Element.prototype.show = function() {
     this.style.visibility = "visible";
+    return this
 };
 
 /**
  * Extends element prototype with a function to toggle an elements visiblity style.
+ * @returns {Element} - Returns the element, allowing for chaining of other utils.
  */
 Element.prototype.toggle = function() {
     this.style.visibility = this.style.visibility=="visible"?"hidden":"visible"
+    return this
 };
