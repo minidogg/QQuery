e033d00c42b2400a14407e3f6cb34d573e90ba25
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-02-23 21:49:27 -0600

    a

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,9 +7,20 @@ var qquery = {
 }
 
 //query.js
+/**
+ * Short hand query selector
+ * @param {string} query - The query selector
+ * @param {bool} multiple - Whether it should use querySelector or querySelectorAll. Default value is true.
+ * @param {Element} el - The element to run the query selector on. Default value is the document
+ */
 function q(query,multiple=false,el=document){
     return multiple?el.querySelectorAll(query):el.querySelector(query);
 };
+/**
+ * Short hand query selector from an element
+ * @param {string} query - The query selector
+ * @param {bool} multiple - Whether it should use querySelector or querySelectorAll. Default value is true.
+ */
 Element.prototype.q = function(query, multiple = false) {
     return q(query, multiple, this);
 };
